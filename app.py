ccf51f1c11b9e2ec469ac8d52b5b4827b25ebe2e
David Sebok <david_sebok@epam.com>
2020-07-02 12:32:21 +0200

    Create initial state
    Virtualenv is included to store the required packages.
    HelloWorld and Authentication is working.
    User table is created in MySQL.

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'David'
+app.config['MYSQL_DATABASE_PASSWORD'] = '20ential'
+app.config['MYSQL_DATABASE_DB'] = 'WordSaver'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+@app.route("/Authenticate")
+def authenticate():
+    username = request.args.get('UserName')
+    password = request.args.get('Password')
+    cursor = mysql.connect().cursor()
+    cursor.execute(
+        "SELECT * from User where Username='" +
+        username +
+        "' and Password='" +
+        password +
+        "'")
+    data = cursor.fetchone()
+    if data is None:
+        return "Username or Password is wrong"
+    else:
+        return "Logged in successfully"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
