5b6fb2c9780de28b38ed36d6a4b0a74a7b325236
David Sebok <david_sebok@epam.com>
2020-07-07 13:22:53 +0200

    Create unit tests for word-checker in word-service

--- tests/word_service_test.py
+++ tests/word_service_test.py
@@ -37,7 +37,7 @@ class WordServiceTest(unittest.TestCase):
         # THEN
         self.assertFalse(result)
 
-    def test_text_check_doesnt_accept_brackets(self):
+    def test_text_check_denies_brackets(self):
         # GIVEN
         text = "[] abcde"
         # WHEN
@@ -45,7 +45,7 @@ class WordServiceTest(unittest.TestCase):
         # THEN
         self.assertTrue(result)
 
-    def test_text_check_doesnt_accept_quotation_marks(self):
+    def test_text_check_denies_quotation_marks(self):
         # GIVEN
         text = "\"abcde\" is a quote"
         # WHEN
@@ -53,7 +53,7 @@ class WordServiceTest(unittest.TestCase):
         # THEN
         self.assertTrue(result)
 
-    def test_text_check_doesnt_accept_plus_signs(self):
+    def test_text_check_denies_plus_signs(self):
         # GIVEN
         text = "add + these + words + together"
         # WHEN
@@ -61,11 +61,107 @@ class WordServiceTest(unittest.TestCase):
         # THEN
         self.assertTrue(result)
 
-    def test_text_check_doesnt_accept_only_whitespace(self):
+    def test_text_check_denies_only_whitespace(self):
         # GIVEN
         text = "   "
         # WHEN
         result = word_service.text_has_invalid_characters(text)
+        # THEN
+        self.assertTrue(result)
+
+    def test_text_check_denies_empty_char(self):
+        # GIVEN
+        text = ""
+        # WHEN
+        result = word_service.text_has_invalid_characters(text)
+        # THEN
+        self.assertTrue(result)
+
+    def test_word_check_accepts_letters(self):
+        # GIVEN
+        word = "abcdefgh"
+        # WHEN
+        result = word_service.word_has_invalid_characters(word)
+        # THEN
+        self.assertFalse(result)
+
+    def test_word_check_accepts_capital_letters_as_well(self):
+        # GIVEN
+        word = "AbCdEfGh"
+        # WHEN
+        result = word_service.word_has_invalid_characters(word)
+        # THEN
+        self.assertFalse(result)
+        
+    def test_word_check_accepts_only_digits(self):
+        # GIVEN
+        word = "0123456789"
+        # WHEN
+        result = word_service.word_has_invalid_characters(word)
+        # THEN
+        self.assertFalse(result)
+
+    def test_word_check_accepts_digits_and_letters_combined(self):
+        # GIVEN
+        word = "A0d1fg2rg3Tgs4gdf5gg6g789xxx"
+        # WHEN
+        result = word_service.word_has_invalid_characters(word)
+        # THEN
+        self.assertFalse(result)
+
+    def test_word_check_denies_prespace(self):
+        # GIVEN
+        word = " prespace"
+        # WHEN
+        result = word_service.word_has_invalid_characters(word)
+        # THEN
+        self.assertTrue(result)
+    
+    def test_word_check_denies_midspace(self):
+        # GIVEN
+        word = "mid space"
+        # WHEN
+        result = word_service.word_has_invalid_characters(word)
+        # THEN
+        self.assertTrue(result)
+
+    def test_word_check_denies_parenthesis(self):
+        # GIVEN
+        word = "(parenthesis)"
+        # WHEN
+        result = word_service.word_has_invalid_characters(word)
+        # THEN
+        self.assertTrue(result)
+
+    def test_word_check_denies_hyphen(self):
+        # GIVEN
+        word = "hy-phen"
+        # WHEN
+        result = word_service.word_has_invalid_characters(word)
+        # THEN
+        self.assertTrue(result)
+
+    def test_word_check_denies_comma(self):
+        # GIVEN
+        word = "comma,"
+        # WHEN
+        result = word_service.word_has_invalid_characters(word)
+        # THEN
+        self.assertTrue(result)
+    
+    def test_word_check_denies_only_space(self):
+        # GIVEN
+        word = "   "
+        # WHEN
+        result = word_service.word_has_invalid_characters(word)
+        # THEN
+        self.assertTrue(result)
+
+    def test_word_check_denies_empty_char(self):
+        # GIVEN
+        word = ""
+        # WHEN
+        result = word_service.word_has_invalid_characters(word)
         # THEN
         self.assertTrue(result)
 
